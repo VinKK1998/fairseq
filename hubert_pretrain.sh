2549ab6940f89fa6867e8c019f4ad755b574b58b
VinKK1998 <xiawentian1998@outlook.com>
2021-11-06 11:01:07 +0800

    undate settings

--- hubert_pretrain.sh
+++ hubert_pretrain.sh
@@ -5,9 +5,9 @@
 #SBATCH -p compute                              # 作业提交的分区为 compute
 #SBATCH -N 1                                    # 作业申请 1 个节点
 #SBATCH -n 1 
-#SBATCH -t 1-00:00:00                            # 任务运行的最长时间为 1 小时
+#SBATCH -t 7-00:00:00                            # 任务运行的最长时间为 1 小时
 #SBATCH -w gpu26                               # 指定运行作业的节点
-#SBATCH --gres=gpu:a100-pcie-40gb:1
+#SBATCH --gres=gpu:a100-pcie-40gb:2
 
 pwd; hostname;
 TIME=$(date -Iseconds)
@@ -18,7 +18,7 @@ conda activate py38-torch
 python -m debugpy --listen 0.0.0.0:55557 fairseq_cli/hydra_train.py \
   --config-dir examples/hubert/config/pretrain \
   --config-name hubert_base_librispeech \
-  task.data=/users10/wtxia/AISHELL-2/hubert_pretrain_prepared_done \
-  task.label_dir=/users10/wtxia/AISHELL-2/hubert_pretrain_prepared_done \
+  task.data=datasets \
+  task.label_dir=datasets \
   task.labels='["km"]' \
   model.label_rate=100
